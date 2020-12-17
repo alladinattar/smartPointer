0cb23f20f1f90cbc1d10c085bce72ba97dc53fcb
rinamuka <rinamuka4@gmail.com>
2020-12-17 04:25:48 -0800

    add 3 tests

--- include/header.hpp
+++ include/header.hpp
@@ -10,18 +10,20 @@ class SharedPtr {
 
  public:
   SharedPtr() : pointerToObject{nullptr}, counter(nullptr){};
-  SharedPtr(T* ptr) : pointerToObject{ptr}, counter(new std::atomic<uint>) {
+  explicit SharedPtr(T* ptr) : pointerToObject{ptr}, counter(new std::atomic_uint) {
     *counter = 1;
   };
   SharedPtr(const SharedPtr& r)
-      : pointerToObject(r.pointerToObject), counter(r.counter){++(*counter)};
+      : pointerToObject(r.pointerToObject), counter(r.counter){++(*counter);};
   SharedPtr(SharedPtr&& r)
-      : pointerToObject(r.pointerToObject), counter(r.counter){++(*counter)};
+        : pointerToObject(r.pointerToObject), counter(r.counter){++(*counter);};
   ~SharedPtr() {
-    if (counter == 1) {
-      delete counter counter = nullptr pointerToObject = nullptr
+    if (*counter == 1) {
+      delete counter;
+      counter = nullptr;
+      pointerToObject = nullptr;
     } else {
-      --(*counter)
+      --(*counter);
     }
   };
   auto operator=(const SharedPtr& r) -> SharedPtr& {
@@ -44,15 +46,15 @@ class SharedPtr {
   };
 
   // проверяет, указывает ли указатель на объект
-  operator bool() const {
+  explicit operator bool() const {
     if (pointerToObject == nullptr) {
       return false;
     } else {
       return true;
     }
   };
-  auto operator*() const -> T& { return *get(); };
-  auto operator->() const -> T* { return get(); };
+  auto operator*() const -> T& { return *pointerToObject; };
+  auto operator->() const -> T* { return pointerToObject; };
 
   auto get() -> T* { return pointerToObject; };
   void reset() {
@@ -71,9 +73,9 @@ class SharedPtr {
     if (counter != nullptr) {
       --(*counter);
     }
-    counter = new std::atomic<uint>;
+    counter = new std::atomic_uint;
     *counter = 1;
-    pointerToObject = ptr
+    pointerToObject = ptr;
   };
   void swap(SharedPtr& r){
       SharedPtr<T> temp(r);
@@ -82,6 +84,6 @@ class SharedPtr {
   };
   // возвращает количество объектов SharedPtr, которые ссылаются на тот же
   // управляемый объект
-  auto use_count() const -> size_t{return *counter};
+  auto use_count() const -> std::size_t{return *counter;};
 };
 #endif  // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,30 @@
 // Copyright 2020 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include "header.hpp"
+TEST(Example, EmptyTest) { EXPECT_TRUE(true); }
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+/*TEST(SharedPtr, constructor1) {
+  SharedPtr<int> constr1;
+  ASSERT_EQ(*constr1, nullptr);
+}*/
+
+TEST(SharedPtr, constructorWithArgument) {
+  int a = 8;
+  SharedPtr<int> var(&a);
+  ASSERT_EQ(*var, a);
+}
+
+TEST(SharedPtr, constructorWithLValLink){
+  int a = 8;
+  SharedPtr var(&a);
+  SharedPtr l(var);
+  ASSERT_EQ(var.use_count(), 2);
+}
+
+TEST(SharedPtr, constructorWithRValLink){
+  int a = 9;
+  SharedPtr var(&a);
+  SharedPtr r(std::move(var));
+  ASSERT_EQ(var.use_count(),2);
 }
